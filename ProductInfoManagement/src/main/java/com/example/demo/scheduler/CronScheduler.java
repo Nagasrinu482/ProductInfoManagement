3b66c61b3646148107c13634e1a24023eba40084
VoidMain1 <VoidMain1@VoidMain1-PC>
2019-05-13 17:53:40 +0530

    done2

---
+++ ProductInfoManagement/src/main/java/com/example/demo/scheduler/CronScheduler.java
@@ -0,0 +1,27 @@
+package com.example.demo.scheduler;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CronScheduler {
+
+	/*
+	 * @Scheduled(cron = "0 * 9 * * ?") public void cronJobSch() { SimpleDateFormat
+	 * sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS"); Date now = new Date();
+	 * String strDate = sdf.format(now);
+	 * System.out.println("Java cron job expression:: " + strDate); }
+	 */
+	
+	@Scheduled(fixedRate = 1000)
+	public void fixedRateSch() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+		Date now = new Date();
+		String strDate = sdf.format(now);
+		System.out.println("Fixed Rate scheduler:: " + strDate);
+	}
+}
